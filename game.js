4cb2aa3b9fe6ff77a78574fe1ccc4272e3cbf447
Joel Encinas <joelencinasmartin@gmail.com>
2023-01-21 18:30:05 +0100

    added base files

---
+++ game.js
@@ -0,0 +1,20 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+
+let currentWeapon = 0;
+let fighting;
+
+let monsterHealth;
+let inventory = ['stick'];
+
+let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
+let button3 = document.querySelector('#button3');
+let text = document.querySelector('#text');
+let xpText = document.querySelector('#xpText');
+let healthText = document.querySelector('#healthText');
+let goldText = document.querySelector('#goldText');
+let monsterStats = document.querySelector('#monsterStats');
+let monsterNameText = document.querySelector('#monsterName');
+let monsterHealthText = document.querySelector('#monsterHealth');
