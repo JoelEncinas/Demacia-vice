fa6c204c8fffa58a4e19f9f1ed1a1c078cc92cec
Joel Encinas <joelencinasmartin@gmail.com>
2023-01-22 06:45:26 +0100

    addded easter egg

--- game.js
+++ game.js
@@ -96,7 +96,7 @@ const locations = [
     {
         name: 'kill monster',
         'button text': ['Go to town square', 'Go to town square', 'Go to town square'],
-        'button functions': [goTown, goTown, goTown],
+        'button functions': [goTown, goTown, easterEgg],
         text: 'The monster screams as it dies. You gain experience points and find gold.'
     },
     {
@@ -110,6 +110,12 @@ const locations = [
         'button text': ['REPLAY?', 'REPLAY?', 'REPLAY?'],
         'button functions': [restart, restart, restart],
         text: 'You defeat the dragon! YOU WIN THE GAME!'
+    },
+    {
+        name: 'easter egg',
+        'button text': ['2', '8', 'Go to town square'],
+        'button functions': [pickTwo, pickEight, goTown],
+        text: 'You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 andd 10. If the number you choose matches one of the random numbers, you win!'
     }
 ];
 
@@ -280,3 +286,41 @@ function restart() {
     xpText.innerText = xp;
     goTown();
 }
+
+function easterEgg() {
+    update(locations[7]);
+}
+
+function pickTwo(){
+    pick(2);
+}
+
+function pickEight(){
+    pick(8);
+}
+
+function pick(guess){
+    let numbers = [];
+    while(numbers.length < 10){
+        numbers.push(Math.floor(Math.random() * 11))
+    }
+
+    text.innerText = 'You picked ' + guess + '. Here are the random numbers:\n';
+
+    for(let i = 0; i < 10; i++){
+        text.innerText += numbers[i] + '\n';
+    }
+
+    if(numbers.indexOf(guess) !== -1){
+        text.innerText += 'Right! You win 200 gold!';
+        gold += 200;
+        goldText.innerText = gold;
+    } else {
+        text.innerText += 'Wrong! You lose 50 health!';
+        health -= 50;
+        healthText.innerText = health;
+        if(health <= 0){
+            lose();
+        }
+    }
+}    
