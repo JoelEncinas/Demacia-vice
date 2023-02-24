ffe483e210ad0b629165a46a153d422a11178b7c
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-24 18:26:55 +0100

    changed placeholder for search, removed dependencies and added about

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5500;
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 // middleware
 app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-let name = 'agurin'
+app.get("/summoner", (req, res) => {
+  res.send("Pings end point");
+});
+
+app.get("/about", (req, res) => {
+  res.send("about end point");
+});
+
+app.get("*", (req, res) => {
+  res.send("404 Page not found");
+});
+
+let name = "agurin";
 const SEARCH_USERNAME = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}`;
 
 fetch(SEARCH_USERNAME, {
-    method: 'GET',
-    headers: {
-        'X-Riot-Token': 'RGAPI-b5c9a3a3-2c02-4119-9057-511fec206b1e'
-    },
+  method: "GET",
+  headers: {
+    "X-Riot-Token": "RGAPI-b5c9a3a3-2c02-4119-9057-511fec206b1e",
+  },
 })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log('Success:', data);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+  .then((response) => response.json())
+  .then((data) => {
+    //console.log("Success:", data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
 
 app.listen(port, () => {
-    console.log(`Success! Your application is running on port ${port}.`);
+  console.log(`Success! Your application is running on port ${port}.`);
 });
