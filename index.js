d84c4a3edf3ef34d72b44595d1a79f5fdbc27bd2
Joel Encinas <joelencinasmartin@gmail.com>
2023-02-24 13:10:28 +0100

    added middleware

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
-  });
+});
 
 app.listen(port, () => {
-  console.log(`Success! Your application is running on port ${port}.`);
+    console.log(`Success! Your application is running on port ${port}.`);
 });
