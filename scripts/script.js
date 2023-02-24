b9c950348fbfce09b3541df8c868744e7f963f91
Joel Encinas <joelencinasmartin@gmail.com>
2023-02-24 11:49:53 +0100

    api call

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,14 @@
+import { RIOT } from '/api';
+
 $(document).ready(() => {
-    API_KEY = process.env.RIOT;
+    API_KEY = RIOT;
 
     URI = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/agurin`
 
     // Request with custom header
     $.ajax({
         url: URI,
-        headers: { "X-Riot-Token": "RGAPI-b5c9a3a3-2c02-4119-9057-511fec206b1e" },
+        headers: { "X-Riot-Token": API_KEY },
         data: JSON.stringify(data),
         contentType: 'application/json',
         async: false,
