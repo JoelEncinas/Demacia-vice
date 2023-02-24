ecf5e3323cbd52a23bc2e8e2e15fafab89b1a376
Joel Encinas <joelencinasmartin@gmail.com>
2023-02-24 13:52:41 +0100

    api call success

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 5500;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
@@ -9,11 +9,29 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/views/index.html");
 });
+
+let name = 'agurin'
+const SEARCH_USERNAME = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}`;
+
+fetch(SEARCH_USERNAME, {
+    method: 'GET',
+    headers: {
+        'X-Riot-Token': 'RGAPI-b5c9a3a3-2c02-4119-9057-511fec206b1e'
+    },
+})
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 
 app.listen(port, () => {
     console.log(`Success! Your application is running on port ${port}.`);
