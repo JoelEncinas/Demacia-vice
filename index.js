45b39490a5b8bc5089d5bd341c07d3549891ff89
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-24 19:24:14 +0100

    created hsb files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5500;
 const cors = require("cors");
+const hsb = require("hbs");
 const summonerData = require("./utils/summoner");
 
 // middleware
